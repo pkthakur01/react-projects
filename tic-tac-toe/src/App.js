c847fc014c13e1cab78ee9393bd604163d4f0bdc
pkthakur <prakash.kumar@simplyfi.tech>
2021-07-15 07:42:22 +0530

    new project added

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Icon from './components/icons';
+
+function App() {
+  return (
+    <div className="App">
+       
+       <Icon/>
+    </div>
+  );
+}
+
+export default App;
