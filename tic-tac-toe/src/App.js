8dcc1048bcf9b07c4dd10aad6fff382670374c79
pkthakur <prakash.kumar@simplyfi.tech>
2022-01-21 12:13:40 +0530

    dfh

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+
 import logo from './logo.svg';
 import './App.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {Card, CardBody, Container, Button,Col, Row} from "reactstrap";                                                                   
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Icon from './components/icons';
 
